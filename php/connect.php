04387a865d46e52b95c7adc33f6b719f5c1d0660
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-02 21:38:09 +0530

    cert added

--- php/connect.php
+++ php/connect.php
@@ -5,12 +5,16 @@ $password = 'Tasty#byte';
 $db_name = 'tastybytedb';
 
 //Establishes the connection
-
+echo "initial";
 
 $con=mysqli_init(); 
-mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); 
-mysqli_real_connect($con, "tastybyte.mysql.database.azure.com", "tastybyte@tastybyte", "Tasty#byte", "tastybytedb", 3306);
 
+echo "initia2l";
+mysqli_ssl_set($con, NULL, NULL, /ssl/BaltimoreCyberTrustRoot.crt.pem, NULL, NULL); 
+
+echo "initial3";
+mysqli_real_connect($con, "tastybyte.mysql.database.azure.com", "tastybyte@tastybyte", "Tasty#byte", "tastybytedb", 3306);
+echo "initial343";
 //Run the Select query
 echo "Reading data from table:";
 $res = mysqli_query($conn, 'SELECT * FROM customers');

6a5a838287c0641847d8c32bf3aa9b4f68eb8b04
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-02 21:05:12 +0530

    echo test

--- php/connect.php
+++ php/connect.php
@@ -11,8 +11,32 @@ mysqli_ssl_set($conn,NULL,NULL, "/ssl/html/BaltimoreCyberTrustRoot.crt.pem", NUL
 
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
+die('Failed to connect to MySQL11: '.mysqli_connect_error());
 }
+
+
+
+
+
+$con=mysqli_connect($conn, $host, $username, $password, $db_name, 3306);
+
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$con=mysqli_init();
+if (!$con)
+  {
+  die("mysqli_init failed");
+  }
+echo "init pass";
+mysqli_ssl_set($con,"key.pem","cert.pem","cacert.pem",NULL,NULL); 
+
+if (!mysqli_real_connect($con,"localhost","user1","datasoft123","hr"))
+  {
+  die("Connect Error: " . mysqli_connect_error());
+  }
+
 
 //Run the Select query
 printf("Reading data from table: \n");

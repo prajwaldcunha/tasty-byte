0710bd64571ddb3eaf96497a0ef4f025bd97c3b3
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-02 19:45:12 +0530

    change path

--- php/connect.php
+++ php/connect.php
@@ -12,7 +12,7 @@ die('Failed to connect to MySQL: '.mysqli_connect_error());
 
 
 $conn = mysqli_init();
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
+mysqli_ssl_set($conn,NULL,NULL, "/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
 
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306, MYSQLI_CLIENT_SSL);
 if (mysqli_connect_errno($conn)) {

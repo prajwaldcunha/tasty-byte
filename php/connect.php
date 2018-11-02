2b9b714fde8f9c433eebae4adc9509f9b24d20e9
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-02 19:48:32 +0530

    path

--- php/connect.php
+++ php/connect.php
@@ -6,6 +6,9 @@ $db_name = 'tastybytedb';
 
 //Establishes the connection
 $conn = mysqli_init();
+
+mysqli_ssl_set($conn,NULL,NULL, "/ssl/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ;
+
 mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());

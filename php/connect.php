3ef054c458021676b68d369b575dd5cafd2b6d33
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-03 07:21:41 +0530

    ssl1

--- php/connect.php
+++ php/connect.php
@@ -10,11 +10,11 @@ echo "initial";
 $con=mysqli_init(); 
 
 
-echo "initial3";
-mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
 echo "initial003";
 mysqli_real_connect($con, "tastybyte.mysql.database.azure.com", "tastybyte@tastybyte", "Tasty#byte", "tastybytedb", 3306,SQLI_CLIENT_SSL);
 
+echo "initial3";
+mysqli_ssl_set($conn,NULL,NULL, "/var/www/html/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }

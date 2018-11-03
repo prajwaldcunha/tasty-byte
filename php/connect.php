448544dde890a048c8ad2be62aaf4519d10616c9
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-03 07:10:11 +0530

    error msg added

--- php/connect.php
+++ php/connect.php
@@ -14,15 +14,20 @@ mysqli_ssl_set($con, NULL, NULL, "/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, N
 
 echo "initial3";
 mysqli_real_connect($con, "tastybyte.mysql.database.azure.com", "tastybyte@tastybyte", "Tasty#byte", "tastybytedb", 3306);
+
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
 echo "initial343";
 //Run the Select query
 echo "Reading data from table:";
 
 
 
-$sql = "SELECT * FROM customers";
-$result = mysqli_query($conn, $sql);
-var_dump($result);
+$res = mysqli_query($conn, 'SELECT * FROM Products');
+while ($row = mysqli_fetch_assoc($res)) {
+var_dump($row);
+}
 
 //Close the connection
 mysqli_close($conn);

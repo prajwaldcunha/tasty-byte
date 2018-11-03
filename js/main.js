df22b43f253c59c81e6fdc8f1cf10810f5a222df
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-03 11:03:02 +0530

    login

--- js/main.js
+++ js/main.js
@@ -238,3 +238,32 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 100000
   });
+
+
+
+  // modal
+
+  $(window, document, undefined).ready(function() {
+
+  $('.input').blur(function() {
+    var $this = $(this);
+    if ($this.val())
+      $this.addClass('used');
+    else
+      $this.removeClass('used');
+  });
+  
+  });
+
+
+$('#tab1').on('click' , function(){
+    $('#tab1').addClass('login-shadow');
+   $('#tab2').removeClass('signup-shadow');
+});
+
+$('#tab2').on('click' , function(){
+    $('#tab2').addClass('signup-shadow');
+   $('#tab1').removeClass('login-shadow');
+
+
+});

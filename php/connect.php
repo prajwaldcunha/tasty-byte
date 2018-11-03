f5757bffa56d26c7d824c561ba7b623e9c81c6b2
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-03 21:37:29 +0530

    Modified connect.php

--- php/connect.php
+++ php/connect.php
@@ -2,37 +2,15 @@
 $host = 'tastybyte.mysql.database.azure.com';
 $username = 'tastybyte@tastybyte';
 $password = 'Tasty#byte';
-$db_name = 'tastybytedb';
+$dbname = 'tastybytedb';
 
-//Establishes the connection
-echo "initial<br>";
+// Create connection
+$conn = new mysqli($host, $username, $password, $dbname);
 
-$con=mysqli_init(); 
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+echo "Connected successfully";
 
-
-echo "initial003<br/>";
-
-
-mysqli_ssl_set($con,NULL,NULL, "/ssl/BaltimoreCyberTrustRoot.crt.pem", NULL, NULL) ; 
-
-mysqli_options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
-mysqli_real_connect($con, "tastybyte.mysql.database.azure.com", "tastybyte@tastybyte", "Tasty#byte", "tastybytedb", 3306,SQLI_CLIENT_SSL);
-
-
-if (mysqli_connect_errno($con)) {
-die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-echo "initial343<br/>";
-//Run the Select query
-echo "Reading data from table:";
-
-
-
-$res = mysqli_query($con, 'SELECT * FROM  customer');
-while ($row = mysqli_fetch_assoc($res)) {
-var_dump($row);
-}
-
-//Close the connection
-mysqli_close($con);
 ?>

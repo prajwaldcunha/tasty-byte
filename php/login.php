6bce7d85eb0d1075e1250553fffe760c064b35df
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-04 18:42:27 +0530

    username problem

--- php/login.php
+++ php/login.php
@@ -22,7 +22,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if ($result->num_rows > 0) {
 	    // output data of each row
 	    while($row = $result->fetch_array()) {
-	        $_SESSION['$username'] = $row['fname'];
+	        $_SESSION['username'] = $row['fname'];
 	    }
 	} else {
 	    $_SESSION['validation_error'] = "Username and password does not match";

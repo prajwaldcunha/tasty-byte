8de5800af8b02df51421f01a79e0aebc619479e0
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-05 16:15:24 +0530

    session start

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-   require 'php/login.php';
+   session_start();
+
 ?>
 
 <!DOCTYPE html>
@@ -708,7 +709,7 @@
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <small>CLOSE </small><span aria-hidden="true">&times;</span>
               </button>
-              <form class="form-horizontal" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method="post">
+              <form class="form-horizontal" action="php/login.php" method="post">
                 <fieldset>
                   <!-- Sign In Form -->
                   <!-- Text input-->

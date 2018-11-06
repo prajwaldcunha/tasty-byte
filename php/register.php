43a0792d9475fb844df462d63c5f16eff6324d37
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-06 18:04:51 +0530

    change

--- php/register.php
+++ php/register.php
@@ -14,7 +14,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $password =md5(cleanInput($_POST["password"]);
 
   
-if(isset($_POST['username'])){
+if(isset($_POST["username"])){
   $sql = "INSERT INTO users (fname, lname,email,phoneno,state,city,locality,password)  VALUES (?,?,?,?,?,?,?,?)";
   $stmt = mysqli_prepare($sql);
   $stmt->bind_param($fname,$lname,$email,$phoneno,$state,$city,$locality,$password);

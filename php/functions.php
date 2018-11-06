7918942b8280217c4e3b3784630fcfdaef604406
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-06 21:40:20 +0530

    Added  real escape string to cleaninput

--- php/functions.php
+++ php/functions.php
@@ -1,8 +1,10 @@
 <?php
+require "connect.php";
 function cleanInput($data) {
 	$data = trim($data);
  	$data = stripslashes($data);
   	$data = htmlspecialchars($data);
+	$data = mysqli_real_escape_string($GLOBALS['conn'], $data);
   	return $data;
 }
 

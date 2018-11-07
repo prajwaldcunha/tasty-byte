7365ed4ecdfbc704cc69da68c5da641c24e44543
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-07 14:53:09 +0530

    font color black

--- php/order.php
+++ php/order.php
@@ -30,9 +30,9 @@ session_start();
 </head>
 <body data-spy="scroll" data-target="#site-navbar" data-offset="200">
 
-	<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light-scrolled" id="site-navbar">
+	<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
 		<div class="container">
-			<a class="navbar-brand" href="../index.php">TastyByte</a>
+			<div class="fontblack"><a class="navbar-brand" href="../index.php">TastyByte</a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="oi oi-menu"></span> Menu
 			</button>
@@ -55,6 +55,7 @@ session_start();
 						</li>
 						<?endif?>
 					</ul>
+				</div>
 				</div>
 			</div>
 		</nav>

ee8b77a8e7518d0208f77e9f7c31f7d77cd26353
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-07 09:47:34 +0530

    REGISTER  file changed

--- php/register.php
+++ php/register.php
@@ -14,19 +14,37 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $locality =cleanInput($_POST["locality"]);
   $password =md5(cleanInput($_POST["password"]));
 
+  if(isempty($email)) {
+		$_SESSION['email_empty_error'] = "email address is empty";
+		// header("Location: https://tastybyte.azurewebsites.net");
+	}
+
+  	if(isempty($password)) {
+		$_SESSION['password_empty_error'] = "password is empty";
+		// header("Location: https://tastybyte.azurewebsites.net");
+	}
+
   $stmt = $GLOBALS[$conn]->prepare("INSERT INTO users (fname, lname,email,phoneno,state,city,locality,password)  VALUES (?,?,?,?,?,?,?,?)"); 
   $stmt->bind_param("ssssssss",$fname,$lname,$email,$phoneno,$state,$city,$locality,$password);
-  $stmt->execute();
+  $result=$stmt->execute();
+// $result = $conn->query($sql);
 
-  	header("Location: https://tastybyte.azurewebsites.net/index.php");
-
- 
- 
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_array()) {
+	        $_SESSION['username'] = $row['fname'];
+	        $_SESSION['name'] = $row['fname'] . " " . $row['lname']; //Full name of user
+	        $_SESSION['uid'] = $row['id'];
+			header("Location: https://tastybyte.azurewebsites.net/index.php");
+	    }
+	} else {
+	    $_SESSION['validation_error'] = "Username and password does not match";
+		$_SESSION['script'] = "<script> $(document).ready(function(){ $('#myModal').modal('show'); }); </script>";
+		header("Location: https://tastybyte.azurewebsites.net/index.php");
+	}
 }
- 
-
-
-
+  	header("Location: https://tastybyte.azurewebsites.net/index.php");
+}
 ?>
 
 

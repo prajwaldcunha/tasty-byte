5feef9e21c3e73f7ecd36c53a01f7b0d7f4cabe4
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-07 08:24:45 +0530

    db query

--- php/addProduct.php
+++ php/addProduct.php
@@ -16,17 +16,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	$userName=$_SESSION['username'];
 
-	$sql="SELECT * FROM USERS";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-    // output data of each row
-    while($row = $result->fetch_assoc()) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
-    }
-} else {
-    echo "0 results";
-}
+	$sql='SELECT * FROM USERS WHERE email="'.$userName.'"';
+	$result = mysqli_query($conn, $sql);
 
-}
+   if (mysqli_num_rows($result) > 0) {
+      while($row = mysqli_fetch_assoc($result)) {
+         echo "Name: " . $row["fname"]. "<br>";
+      }
+   } else {
+      echo "0 results";
+   }
+   mysqli_close($conn);
 
 ?>

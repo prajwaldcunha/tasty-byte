c4d22bedb1659b6b74e89d84367b91a79d252e47
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-08 16:49:58 +0530

    Added JavaScript files to Myprduct.php

--- php/myProduct.php
+++ php/myProduct.php
@@ -134,7 +134,22 @@ $result = $conn->query($sql);
 		</div>
 	</section>
 
-
+	<div id="site-loader" class="show fullscreen">
+		<svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
+		</svg>
+	</div>
+		<script src="../js/anime.min.js"></script>
+		<script src="../js/jquery.min.js"></script>
+		<script src="../js/popper.min.js"></script>
+		<script src="../js/bootstrap.min.js"></script>
+		<script src="../js/jquery.easing.1.3.js"></script>
+		<script src="../js/jquery.waypoints.min.js"></script>
+		<script src="../js/owl.carousel.min.js"></script>
+		<script src="../js/jquery.magnific-popup.min.js"></script>
+		<script src="../js/bootstrap-datepicker.js"></script>
+		<script src="../js/jquery.timepicker.min.js"></script>
+		<script src="../js/jquery.animateNumber.min.js"></script>
+		<script src="../js/main.js"></script>
 
 
 	

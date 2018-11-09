722f70b3109c88cb6e55386e13ea5e2a8c336d10
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-09 11:35:06 +0530

    added css files

--- php/order.php
+++ php/order.php
@@ -24,6 +24,8 @@ session_start();
 
 	<link rel="stylesheet" href="../css/icomoon.css">
 	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/grid_layout.css">
+  <link rel="stylesheet" type="text/css" href="../css/deco.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 </head>

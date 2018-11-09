4420b357e5c98cfca4a420107f1d217b84df69c9
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-09 13:03:51 +0530

    Testing orders

--- php/order.php
+++ php/order.php
@@ -87,7 +87,7 @@ session_start();
 
 									<!-- Modal -->
 									<div class="modal fade" id="ModalDetails" role="dialog">
-										<div class="modal-dialog modal-sm">
+										<div class="modal-dialog modal-md">
 											<div class="modal-content">
 												<div class="modal-header">
 													<button type="button" class="close" data-dismiss="modal">&times;</button>

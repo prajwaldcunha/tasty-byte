857509a7f84a05c40bdcb7d34fb30931e8fbda22
Prajwal Dcunha <dcunhaprajwal@gmail.com>
2018-11-10 07:16:07 +0530

    fix

--- php/myProduct.php
+++ php/myProduct.php
@@ -164,7 +164,7 @@ $result = $conn->query($sql);
 					</div>
 				</section>
 				<!-- END section -->
-			<?php endif; 
+			<?php endif; ?>
 
 			<div id="site-loader" class="show fullscreen">
 			<svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>

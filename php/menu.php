7c3dea55a575ca2625c7c1aaf11fb87c1f36e3b8
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-10 10:41:48 +0530

    Added menu

--- php/menu.php
+++ php/menu.php
@@ -34,7 +34,9 @@ require 'functions.php';
 	<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
 		<div class="container">
 			<a class="navbar-brand" href="../index.php">TastyByte</a>
-		
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button
 			
 			<div class="collapse navbar-collapse" id="site-nav">
 				<ul class="navbar-nav ml-auto">

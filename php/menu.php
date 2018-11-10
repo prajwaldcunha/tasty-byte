313d95f4800589f239c53680e17c63ac1c6cdfc8
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-10 12:47:48 +0530

    menu.php

--- php/menu.php
+++ php/menu.php
@@ -1,19 +1,17 @@
 <?php
-session_start();
 require  'connect.php';
 require 'functions.php';
+session_start();
+
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>TastyByte</title>
+	<title>My Products</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="../css/googleapis.css" rel="stylesheet">
-	
-	<link rel="stylesheet" href="../css/menu.css">
 	<link rel="stylesheet" href="../css/bootstrap.min.css">
 	<link rel="stylesheet" href="../css/open-iconic-bootstrap.min.css">
 	<link rel="stylesheet" href="../css/animate.css">
@@ -24,145 +22,193 @@ require 'functions.php';
 
 	<link rel="stylesheet" href="../css/bootstrap-datepicker.css">
 	<link rel="stylesheet" href="../css/jquery.timepicker.css">
+
 	<link rel="stylesheet" href="../css/icomoon.css">
 	<link rel="stylesheet" href="../css/style.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
+	<link rel="stylesheet" href="../css/product.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script>
+		function redir() {
+			window.location = "https://tastybyte.azurewebsites.net/index.php";
+		}
+		
+				$(document).ready(function() { 
+				$("#channel").change(function(){
+    			$.post( "menu.php", { channel: $(this).val() })
+    			 .success(function(data) {
+
+             $(".result").html(data);
+         });
+   });
+});
+</script>
+	</script>
 </head>
-<body data-spy="scroll" data-target="#site-navbar" data-offset="200">
+
+<body data-spy="scroll" data-target="#site-navbar" data-offset="150">
 
 	<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
 		<div class="container">
-			<a class="navbar-brand" href="../index.php">TastyByte</a>
-		
-			
+			<a class="navbar-brand" href="../index.php"><div style="color: black;">TastyByte</div></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button
+
 			<div class="collapse navbar-collapse" id="site-nav">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item active"><a href="../index.php" class="nav-link active">Home</a></li>
-				
-					<?php if (isset($_SESSION['username'])):?>
+					<li class="nav-item active"><a href="menu.php" class="nav-link active">Menu</a></li>
+
+					<?php if (isset($_SESSION['username'])):
+						
+
+						?>
+
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-								<?php echo $_SESSION['username'];?>	
+								<?php echo $_SESSION['username'];?>
 							</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-								<a class="dropdown-item" href="myProduct.php">My Products</a>
 								<a class="dropdown-item" href="order.php">My Orders</a>
 								<div class="dropdown-divider"></div>
 								<a class="dropdown-item" href="logout.php">Logout</a>
 							</div>
 						</li>
-						<?php endif;?>
-					</ul>
-				</div>
+					<?php endif;?>
+				</ul>
 			</div>
-		</nav>
-		<!-- END nav -->
-	
-		<!--Actual Display-->
-		<section class="site-section bg-light" id="section-order">
+		</div>
+	</nav>
+	<!-- END nav -->
 
-						<div class="container">
-						<form>
-							<select id="myDropDown" name="city" onchange="this.form.submit()">>
-							<option>Choose City</option>
+
+
+
+	<section class="site-section bg-light" id="section-order">
+		<div class="container">
+			<div class="row">
+				<h2>Menu</h2>	
+				<p>
+							<select name='channel'>
+   							<option selected='true' disabled='true'> Choose one channel </option>
 							<option value="Mangalore">Mangalore</option>
 							<option value="Bangalore">Bangalore</option>
 							<option value="Mumbai">Mumbai</option>
 						</select>
-						</form>
 					
-
-					<?php
-
-						if(isset($_GET['city'])){
-							$city=cleanInput($_GET['city']);
-      						if (isset($_SESSION['username'])){
-													$sql = "SELECT name,price,imageurl,details,quantity,price,manufacturedate,pickup_address,products.city,fname,lname,email,phoneno,orderdate FROM products, users,orders WHERE products.uid=users.id AND orders.pid=products.id AND products.city=".$city;
+						</p>
+						<?php 
+								if (isset($_SESSION['username'])):
+								if(isset($_POST['channel'])){
+														$city =cleanInput( $_POST['channel']);
+								$sql  = "SELECT name,price,imageurl,details,quantity,price,manufacturedate,pickup_address,products.city,fname,lname,email,phoneno,orderdate FROM products, users,orders WHERE products.uid=users.id AND orders.pid=products.id AND products.city=".$city;
 													$result = $conn->query($sql);
-															}
-      									}								
-												
-					?>
-
-					<div class="card-deck">
+								}
+							?>	
+			</div>
+			<br/>
+		</div>
+		<div class="container-fluid">
+			<div class="row">
+				<div class="card-deck">
 					<?php
 					$i=0;
-					if ($result->num_rows > 0) {
+					if ($result->num_rows > 0):
    						 // output data of each row
-						while($row = $result->fetch_array()) {
-							$i+=1;
-							if($i%3==0) echo "<br/>";
-					?>
-					<div class="card">
-						<img class="card-img-top" src="../images/offer_1.jpg" alt="Card image cap">
-						<div class="card-body">
-							<h5 class="card-title"><?php echo $row['name']; ?></h5>
-							<p class="card-text">Order date : <?php echo $row['orderdate']; ?></p>
-							<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDetails">Details</button>
-						</div>
+						while($row = $result->fetch_array()):
+							$i++;
+							
+							$time=strtotime($row['manufacturedate']);
+							$mdate=date('m/d/Y',$time);
+							
+							
+							
+							?>
+							<div class="card">
+								<img class="card-img-top" src="<?php echo $row['imageurl'];?>" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo $row['name']; ?></h5>
+									<p class="card-text">Manufacture date : <?php echo $mdate; ?></p>
+									<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDetails<?php echo $i;?>">Details</button>
+								</div>
 
 
-						<!-- Button trigger modal -->
+								<!-- Button trigger modal -->
 
 
-						<!-- Modal -->
-						<div class="modal fade" id="modalDetails" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
-							<div class="modal-dialog modal-dialog-centered" role="document">
-								<div class="modal-content">
-									<div class="modal-header">
-										<h5 class="modal-title" id="exampleModalLongTitle">Product Details</h5>
-										<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-											<span aria-hidden="true">&times;</span>
-										</button>
-									</div>
-									<div class="modal-body">
-										<ul>
-											<li>Product Name : <?php echo $row['name']; ?></li>
-											<li>Price :  Rs. <?php echo $row['price']; ?></li>
-											<li>Quantity: <?php echo $row['quantity']; ?></li>
-											<li>Date of manufacture : <?php echo $row['manufacturedate']; ?></li>
-											<li>Seller Name : <?php echo $row['fname']." ".$row['lname']; ?></li>
-											<li>PickUp Address : <?php echo $row['']; ?></li>
-											<li>Phone number : <?php echo $row['phoneno']; ?></li>
-											<li>Email id: <?php echo $row['email']; ?></li>
-										</ul>
-									</div>
-									<div class="modal-footer">
-										<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								<!-- Modal -->
+								<div class="modal fade" id="modalDetails<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLongTitle">Product Details</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<ul>
+													<li>Product Name : <?php echo $row['name']; ?></li>
+													<li>Price :  Rs. <?php echo $row['price']; ?></li>
+													<li>Quantity: <?php echo $row['quantity']; ?></li>
+													<li>Date of manufacture : <?php echo $mdate; ?></li>
+													<li>PickUp Address : <?php echo $row['pickup_address']; ?></li>
+													
+												</ul>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
-						</div>
-					</div>
-		</div>
-            </div>
-	</section>
-		<!-- End of actual Display -->
-		<?php
-			
-			
-						}
-						}
-		?>
+							<?php
+							if($i%3==0):
+								?>
+							</div>
+							<div class="row">
+								
+								<?php
+							endif;
+							
+						endwhile;
 
-		<!-- loader -->
-	<div id="site-loader" class="show fullscreen">
-		<svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
-		</svg>
-	</div>
-		<script src="../js/anime.min.js"></script>
-		<script src="../js/jquery.min.js"></script>
-		<script src="../js/popper.min.js"></script>
-		<script src="../js/bootstrap.min.js"></script>
-		<script src="../js/jquery.easing.1.3.js"></script>
-		<script src="../js/jquery.waypoints.min.js"></script>
-		<script src="../js/owl.carousel.min.js"></script>
-		<script src="../js/jquery.magnific-popup.min.js"></script>
-		<script src="../js/bootstrap-datepicker.js"></script>
-		<script src="../js/jquery.timepicker.min.js"></script>
-		<script src="../js/jquery.animateNumber.min.js"></script>
-		<script src="../js/main.js"></script>
-		<script src="../js/menu.js"></script>
-	</body>
-</html>	
+						else:?>
+							<p>There are no products in this area</p>
+							<div class="container">
+								<br/>
+								<button type="button" class="btn btn-primary" onclick="redir();">Add Now!</button>
+							</div>
+						<?php endif;?>
+
+					</div>
+				</section>
+				<!-- END section -->
+			<?php endif; ?>
+
+			<div id="site-loader" class="show fullscreen">
+			<svg class="circular" width="48px" height="48px"><circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee"/><circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10" stroke="#F96D00"/>
+			</svg>
+			</div>
+			<script src="../js/anime.min.js"></script>
+			<script src="../js/jquery.min.js"></script>
+			<script src="../js/popper.min.js"></script>
+			<script src="../js/bootstrap.min.js"></script>
+			<script src="../js/jquery.easing.1.3.js"></script>
+			<script src="../js/jquery.waypoints.min.js"></script>
+			<script src="../js/owl.carousel.min.js"></script>
+			<script src="../js/jquery.magnific-popup.min.js"></script>
+			<script src="../js/bootstrap-datepicker.js"></script>
+			<script src="../js/jquery.timepicker.min.js"></script>
+			<script src="../js/jquery.animateNumber.min.js"></script>
+			<script src="../js/main.js"></script>
+
+
+
+			</body>
+			</html>
+
+

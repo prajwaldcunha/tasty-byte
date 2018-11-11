9b985b1a025345d8622867beba8f9801a4671d44
joslin aniseth dcunha <joslindcunha@gmail.com>
2018-11-11 22:46:24 +0530

    responsive design menu

--- php/menu.php
+++ php/menu.php
@@ -63,7 +63,9 @@ session_start();
 	<nav class="navbar navbar-expand-lg navbar-dark site_navbar bg-dark site-navbar-light" id="site-navbar">
 		<div class="container">
 			<a class="navbar-brand" href="../index.php">TastyByte</a>
-			
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#site-nav" aria-controls="site-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
 			<div class="collapse navbar-collapse" id="site-nav">
 				<ul class="navbar-nav ml-auto">
 					<li class="nav-item active"><a href="../index.php" class="nav-link active">Home</a></li>

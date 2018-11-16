ecd1710b3961b0438814bdd03efcdf7db110afb3
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-11-16 23:31:50 +0530

    Final test

--- php/displayMenu.php
+++ php/displayMenu.php
@@ -29,119 +29,237 @@ session_start();
 
 <body>
 	<?php
-	$city = cleanInput($_GET['city']);
-	$sql  = "SELECT products.id as pid,name,imageurl,details,quantity,price,manufacturedate,pickup_address,products.city,fname,lname,email,phoneno FROM products, users WHERE products.uid=users.id AND products.city='".$city . "' AND soldout = 0 AND products.uid NOT IN (SELECT users.id FROM users WHERE users.id = " . $_SESSION['uid'] . ")";
-	$result = $conn->query($sql);
-	?>
+	if (isset($_SESSION['username'])):
+		$city = cleanInput($_GET['city']);
+		$sql  = "SELECT products.id as pid,name,imageurl,details,quantity,price,manufacturedate,pickup_address,products.city,fname,lname,email,phoneno FROM products, users WHERE products.uid=users.id AND products.city='".$city . "' AND soldout = 0 AND products.uid NOT IN (SELECT users.id FROM users WHERE users.id = " . $_SESSION['uid'] . ")";
+		$result = $conn->query($sql);
+		?>
+		
+		<div class="container">
+			<div class="row">
+				<?php
+				$i=0;
+				if ($result->num_rows > 0):
+	   						 // output data of each row
+					while($row = $result->fetch_array()):
+						$i++;
+						$_SESSION['pid']=$row['pid'];
 	
-	<div class="container">
-		<div class="row">
-			<?php
-			$i=0;
-			if ($result->num_rows > 0):
-   						 // output data of each row
-				while($row = $result->fetch_array()):
-					$i++;
-					$_SESSION['pid']=$row['pid'];
-
-					$time=strtotime($row['manufacturedate']);
-					$mdate=date('m/d/Y',$time);
-					?>
-					<div class="col-sm-4">
-						<div class="card">
-							<img class="card-img-top" src="<?php echo $row['imageurl'];?>" alt="Card image cap">
-							<div class="card-body">
-								<h5 class="card-title"><?php echo $row['name']; ?></h5>
-								<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDetails<?php echo $i;?>">Details</button>
-								<?php
-								if (isset($_SESSION['username'])):?>
-								<form method="POST" action="writeOrder.php">
-								<input type="submit" name="btnOrder" class="btn btn-primary" value="Order Now!" ></input>
-								</form>
-								<?php 
-								else:?>
-								<button type="button" class="btn btn-primary" id="myBtn" onclick="alertfunc()">Order now</button>
+						$time=strtotime($row['manufacturedate']);
+						$mdate=date('m/d/Y',$time);
+						?>
+						<div class="col-sm-4">
+							<div class="card">
+								<img class="card-img-top" src="<?php echo $row['imageurl'];?>" alt="Card image cap">
+								<div class="card-body">
+									<h5 class="card-title"><?php echo $row['name']; ?></h5>
+									<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDetails<?php echo $i;?>">Details</button>
+									<?php
+									if (isset($_SESSION['username'])):?>
+									<form method="POST" action="writeOrder.php">
+									<input type="submit" name="btnOrder" class="btn btn-primary" value="Order Now!" ></input>
+									</form>
+									<?php 
+									else:?>
+									<button type="button" class="btn btn-primary" id="myBtn" onclick="alertfunc()">Order now</button>
+									
+									<?php endif;?>
+									
+								</div>
 								
-								<?php endif;?>
 								
-							</div>
-							
-							
-							
-							<!--Order food modal-->
-							<div class="modal fade" id="modalEnterOrder1" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title" id="exampleModalLongTitle">Order Details</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">
-											<p><?php echo $_SESSION['orderInfo'];?></p>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-											
+								
+								<!--Order food modal-->
+								<div class="modal fade" id="modalEnterOrder1" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLongTitle">Order Details</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<p><?php echo $_SESSION['orderInfo'];?></p>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+												
+											</div>
 										</div>
 									</div>
 								</div>
-							</div>
-							<!--end-->
-
-
-							<!-- Button trigger modal -->
-
-
-							<!-- Modal -->
-							<div class="modal fade" id="modalDetails<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
-								<div class="modal-dialog modal-dialog-centered" role="document">
-									<div class="modal-content">
-										<div class="modal-header">
-											<h5 class="modal-title" id="exampleModalLongTitle">Product Details</h5>
-											<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-												<span aria-hidden="true">&times;</span>
-											</button>
-										</div>
-										<div class="modal-body">
-											<ul>
-												<li>Product Name : <?php echo $row['name']; ?></li>
-												<li>Price :  Rs. <?php echo $row['price']; ?></li>
-												<li>Quantity: <?php echo $row['quantity']; ?></li>
-												<li>Date of manufacture : <?php echo $mdate; ?></li>
-												<li>Seller Name : <?php echo $row['fname']." ".$row['lname']; ?></li>
-												<li>PickUp Address : <?php echo $row['pickup_address']; ?></li>
-												<li>Phone number : <?php echo $row['phoneno']; ?></li>
-												<li>Email id: <?php echo $row['email']; ?></li>
-											</ul>
-										</div>
-										<div class="modal-footer">
-											<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+								<!--end-->
+	
+	
+								<!-- Button trigger modal -->
+	
+	
+								<!-- Modal -->
+								<div class="modal fade" id="modalDetails<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
+									<div class="modal-dialog modal-dialog-centered" role="document">
+										<div class="modal-content">
+											<div class="modal-header">
+												<h5 class="modal-title" id="exampleModalLongTitle">Product Details</h5>
+												<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+													<span aria-hidden="true">&times;</span>
+												</button>
+											</div>
+											<div class="modal-body">
+												<ul>
+													<li>Product Name : <?php echo $row['name']; ?></li>
+													<li>Price :  Rs. <?php echo $row['price']; ?></li>
+													<li>Quantity: <?php echo $row['quantity']; ?></li>
+													<li>Date of manufacture : <?php echo $mdate; ?></li>
+													<li>Seller Name : <?php echo $row['fname']." ".$row['lname']; ?></li>
+													<li>PickUp Address : <?php echo $row['pickup_address']; ?></li>
+													<li>Phone number : <?php echo $row['phoneno']; ?></li>
+													<li>Email id: <?php echo $row['email']; ?></li>
+												</ul>
+											</div>
+											<div class="modal-footer">
+												<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+											</div>
 										</div>
 									</div>
 								</div>
 							</div>
 						</div>
-					</div>
-					<?php
-					if($i%3==0):
-						?>
-					</div>
-					<br/>
-					<?php if($result->num_rows != $i):?>
-						<div class="row">
-							<?php
+						<?php
+						if($i%3==0):
+							?>
+						</div>
+						<br/>
+						<?php if($result->num_rows != $i):?>
+							<div class="row">
+								<?php
+							endif;
 						endif;
-					endif;
-
-				endwhile;
-
-				else:?>
-				<p>There are no products in your area</p>
-			<?php endif;?>
-
-		</div>
-
+	
+					endwhile;
+	
+					else:?>
+					<p>There are no products in your area</p>
+				<?php endif;?>
+	
+			</div>
+			
+			
+			
+		<?php else:
+			$city = cleanInput($_GET['city']);
+			$sql  = "SELECT products.id as pid,name,imageurl,details,quantity,price,manufacturedate,pickup_address,products.city,fname,lname,email,phoneno FROM products, users WHERE products.uid=users.id AND products.city='".$city . "' AND soldout = 0";
+			$result = $conn->query($sql);
+			?>
+			
+			<div class="container">
+				<div class="row">
+					<?php
+					$i=0;
+					if ($result->num_rows > 0):
+		   						 // output data of each row
+						while($row = $result->fetch_array()):
+							$i++;
+							$_SESSION['pid']=$row['pid'];
+		
+							$time=strtotime($row['manufacturedate']);
+							$mdate=date('m/d/Y',$time);
+							?>
+							<div class="col-sm-4">
+								<div class="card">
+									<img class="card-img-top" src="<?php echo $row['imageurl'];?>" alt="Card image cap">
+									<div class="card-body">
+										<h5 class="card-title"><?php echo $row['name']; ?></h5>
+										<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#modalDetails<?php echo $i;?>">Details</button>
+										<?php
+										if (isset($_SESSION['username'])):?>
+										<form method="POST" action="writeOrder.php">
+										<input type="submit" name="btnOrder" class="btn btn-primary" value="Order Now!" ></input>
+										</form>
+										<?php 
+										else:?>
+										<button type="button" class="btn btn-primary" id="myBtn" onclick="alertfunc()">Order now</button>
+										
+										<?php endif;?>
+										
+									</div>
+									
+									
+									
+									<!--Order food modal-->
+									<div class="modal fade" id="modalEnterOrder1" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
+										<div class="modal-dialog modal-dialog-centered" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLongTitle">Order Details</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+														<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-body">
+													<p><?php echo $_SESSION['orderInfo'];?></p>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+													
+												</div>
+											</div>
+										</div>
+									</div>
+									<!--end-->
+		
+		
+									<!-- Button trigger modal -->
+		
+		
+									<!-- Modal -->
+									<div class="modal fade" id="modalDetails<?php echo $i;?>" tabindex="-1" role="dialog" aria-labelledby="modalDetailsTitle" aria-hidden="true">
+										<div class="modal-dialog modal-dialog-centered" role="document">
+											<div class="modal-content">
+												<div class="modal-header">
+													<h5 class="modal-title" id="exampleModalLongTitle">Product Details</h5>
+													<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+														<span aria-hidden="true">&times;</span>
+													</button>
+												</div>
+												<div class="modal-body">
+													<ul>
+														<li>Product Name : <?php echo $row['name']; ?></li>
+														<li>Price :  Rs. <?php echo $row['price']; ?></li>
+														<li>Quantity: <?php echo $row['quantity']; ?></li>
+														<li>Date of manufacture : <?php echo $mdate; ?></li>
+														<li>Seller Name : <?php echo $row['fname']." ".$row['lname']; ?></li>
+														<li>PickUp Address : <?php echo $row['pickup_address']; ?></li>
+														<li>Phone number : <?php echo $row['phoneno']; ?></li>
+														<li>Email id: <?php echo $row['email']; ?></li>
+													</ul>
+												</div>
+												<div class="modal-footer">
+													<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<?php
+							if($i%3==0):
+								?>
+							</div>
+							<br/>
+							<?php if($result->num_rows != $i):?>
+								<div class="row">
+									<?php
+								endif;
+							endif;
+		
+						endwhile;
+		
+						else:?>
+						<p>There are no products in your area</p>
+					<?php endif;?>
+		
+				</div>
+		<?php endif;?>
 	</body>
 	</html>

899b5fb40d0bfa7325b971b05e0ad316b0b4f881
Raghavendra Bhat <s.bhatraghavendra@gmail.com>
2018-12-05 16:11:42 +0530

    Minor changes

--- php/order.php
+++ php/order.php
@@ -29,7 +29,6 @@ session_start();
 	<link rel="icon" type="image/png" sizes="32x32" href="../images/icons2/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="../images/icons2/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="../images/icons2/favicon-16x16.png">
-	<link rel="manifest" href="../images/icons2/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
 	<meta name="msapplication-TileImage" content="../images/icons2/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
